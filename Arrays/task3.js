19dd340aeac41792e346650ed6e15e5d4fa468ff
afurtune64 <afurtune@64labs.com>
2025-04-19 16:23:33 +0300

    HW-8.1

---
+++ Arrays/task3.js
@@ -0,0 +1,7 @@
+const arrayNumbers = [10, 20, 30, 40, 50];
+const initialValue = 0;
+const sum = arrayNumbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, initialValue);
+
+console.log("Сума елементів масиву:", sum);
