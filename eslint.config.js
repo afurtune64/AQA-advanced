9672a09fc63eee6eae8e1863dd5fed4eacdd2875
afurtune64 <afurtune@64labs.com>
2025-04-29 20:55:00 +0300

    HW-12.1

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: ["prettier"],"rules": {"prettier/prettier": "error"}, 
+  extends: ['eslint:recommended', 'prettier'] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
+
